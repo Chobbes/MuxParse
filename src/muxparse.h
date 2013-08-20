09af161397f0f8611718ec062f3a8626597f0a7a
Calvin Beck <hobbes@ualberta.ca>
2013-08-20 11:01:32 -0600

    Added the declaration for the main parsing function. This file gives the main interface of the MuxParse library.

---
+++ src/muxparse.h
@@ -0,0 +1,51 @@
+/* Copyright (C) 2013 Calvin Beck
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#ifndef MUXPARSE_H
+#define MUXPARSE_H
+
+#include <stdio.h>
+#include <mux_pipe.h>  /* Need the MuxPipe structure! */
+
+
+/*
+  Arguments:
+      mux_file: File pointer for the .mux file that we are reading.
+
+      pipe: Pointer to a pipe structure to store results in.
+
+  Returns 0 on success, 1 when the end of the file has been reached,
+  and 2 if there was a parse error.
+
+  Note that the mux_file should have just been opened before calling
+  this the first time without manipulating the file position
+  indicator, and only this function should be used to parse the entire
+  file. It relies upon the file position indicator staying put! Moving
+  it manually may cause interesting results.
+
+ */
+
+int mux_parse_pipe(FILE *mux_file, MuxPipe *pipe);
+
+#endif
